ed93cb8d98d07a967843cb8d896da121b0d3adc4
sornav <sornavxfx021@gmail.com>
2020-12-19 23:20:59 +0530

    Project Updated

--- Project.py
+++ Project.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[9]:
 
 
-import cv2
+import os
+from pygame import mixer
+import time
 from tensorflow.keras.models import load_model
 import numpy as np
+import cv2
+path = os.getcwd()
+mixer.init()
+sound = mixer.Sound('alarm.wav')
 face = cv2.CascadeClassifier('haar cascade files\haarcascade_frontalface_alt.xml')
 leye = cv2.CascadeClassifier('haar cascade files\haarcascade_lefteye_2splits.xml')
 reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xml')
-model1 = load_model('./Models/Own_model_Left_Eye_Detection(Best).h5')
-model2 = load_model('./Models/Own_model_Right_Eye_Detection(Best).h5')
+model1 = load_model('./Model/Own_model_Left_Eye_Detection(Best).h5')
+model2 = load_model('./Model/Own_model_Right_Eye_Detection(Best).h5')
 cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_TRIPLEX
 rpred=[99]
 lpred=[99]
+count=0
+score=0
+thicc=2
 while(True):
     ret, frame = cap.read()
+    height,width = frame.shape[:2] 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face.detectMultiScale(gray,minNeighbors=5,scaleFactor=1.1,minSize=(25,25))
     left_eye = leye.detectMultiScale(gray)
@@ -43,20 +53,49 @@ while(True):
         lpred = model1.predict_classes(l_eye)
         break
     if(rpred[0]==0 and lpred[0]==0):
-        cv2.putText(frame,"Eyes Closed",(10,frame.shape[:2][0]-20), font, 1,(0,0,255),1,cv2.LINE_AA)
+        score=score+1
+        cv2.putText(frame,"Closed",(10,height-20), font, 1,(0,0,255),1,cv2.LINE_AA)
     else:
-        cv2.putText(frame,"Eyes Open",(10,frame.shape[:2][0]-20), font, 1,(0,255,0),1,cv2.LINE_AA)
-    cv2.imshow('My Face',frame)
+        score=score-1
+        cv2.putText(frame,"Open",(10,height-20), font, 1,(34,139,34),1,cv2.LINE_AA)
+    
+        
+    if(score<0):
+        score=0   
+    cv2.putText(frame,'  Score:'+str(score),(100,height-20), font, 1,(0,255,255),1,cv2.LINE_AA)
+    if(score>15):
+        
+        cv2.imwrite(os.path.join(path,'image.jpg'),frame)
+        try:
+            sound.play()
+            cv2.putText(frame,"Drowsy",(200,200),font,2,(0,0,255),1)       
+        except: 
+            pass
+        if(thicc<16):
+            thicc= thicc+2
+        else:
+            thicc=thicc-2
+            if(thicc<2):
+                thicc=2
+        cv2.rectangle(frame,(0,0),(width,height),(0,0,255),thicc) 
+    cv2.imshow('frame',frame)
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
 
 
-# In[ ]:
+# In[6]:
 
 
+get_ipython().system('jupyter nbconvert --to script config_template.ipynb')
 
+
+# In[10]:
+
+
+get_ipython().system('jupyter nbconvert --to script Project.ipynb')
 
 
 # In[ ]:
